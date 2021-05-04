6416378d12372a54bab6195b29b82c667283ae7e
ytakky2014 <y.takky2014@gmail.com>
2021-05-04 12:16:02 +0900

    add statuscode-headers

--- section3/simpleget/main.go
+++ section3/simpleget/main.go
@@ -19,4 +19,8 @@ func main() {
 		panic(err)
 	}
 	log.Println(string(body))
+	log.Println("Status:", resp.Status)
+	log.Println("StatusCode:", resp.StatusCode)
+	log.Println("Headers:", resp.Header)
+	log.Println("Content-Length:", resp.Header.Get("Content-Length"))
 }
